40b7d27ce79f89559d28b41c03ca4201f6742e83
m.timurshin <m.timurshin@ittechnogroup.ru>
2022-10-19 14:00:43 +0300

    Задание 3

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return view('hello');
+});
+
+Route::get('/info', function () {
+    return view('info');
+});
+
+Route::get('/news', function () {
+    return view('news');
+});
